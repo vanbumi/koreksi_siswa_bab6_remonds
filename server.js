0f9d3bc19d960920d13f1fa1e53aba20a9d11d91
vanbumi <jsp.dio@gmail.com>
2020-07-06 19:02:59 +0700

    Initial & update koneksi db

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./config/db');
+
+// call connectDB
+connectDB();
+
+app.get('/', (req, res) => res.json('API is Work!'));
+
+// define Routes
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('server running on PORT 5000'));
